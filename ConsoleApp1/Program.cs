669795040ba755e56c4283bd41ee0580fb8b3771
hackerFaruk <onuralparslan97@gmail.com>
2024-02-20 21:31:52 +0300

    added keydetectioın

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -20,6 +20,7 @@ internal class Program
             { "sumfive", 2 },
             { "exit", 3 },
             { "sumoddeven", 4 },
+            {"keydetetc" ,5 },
             
         };
 
@@ -58,6 +59,9 @@ internal class Program
             case 4:
                 sumoddeven();
                 break;
+            case 5:
+                //keydetect();
+                break;
             
         }
 
@@ -77,6 +81,30 @@ internal class Program
             Console.WriteLine(option.Key);
         }
     }
+
+
+
+    public static void keydetect()
+    {
+        ConsoleKeyInfo cki;
+        // Prevent example from ending if CTL+C is pressed.
+        Console.TreatControlCAsInput = true;
+
+        Console.WriteLine("Press any combination of CTL, ALT, and SHIFT, and a console key.");
+        Console.WriteLine("Press the Escape (Esc) key to quit: \n");
+        do
+        {
+            cki = Console.ReadKey();
+            Console.Write(" --- You pressed ");
+            if ((cki.Modifiers & ConsoleModifiers.Alt) != 0) Console.Write("ALT+");
+            if ((cki.Modifiers & ConsoleModifiers.Shift) != 0) Console.Write("SHIFT+");
+            if ((cki.Modifiers & ConsoleModifiers.Control) != 0) Console.Write("CTL+");
+            Console.WriteLine(cki.Key.ToString());
+        } while (cki.Key != ConsoleKey.Escape);
+    }
+
+
+
 
     private static void compare()
     {
