ed13afd2634ceb735e27b5fb272fd927f79e97d7
hackerFaruk <onuralparslan97@gmail.com>
2024-02-20 23:57:12 +0300

    added dungeon crawler nad our hero to screen

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -110,28 +110,47 @@ internal class Program
 
     public static void dungeonCrawler(){
       ConsoleKeyInfo cki;
-      printDungeon();
+      int heroX=4;
+      int heroY = 4;
+
+      printDungeon(heroX, heroY);
 
 
 
     }
-    public static void printDungeon(){
+    public static void printDungeon(int heroX , int heroY){
        
     int dungeonWidth = 7 ;
     int dungeonHeight = 7 ;
 
     string horizontalWall =  new string('-', dungeonWidth);
     // used single ' ' to indicate char 
-    string verticalWall = "|";
-    string emptySpace = "_";
-    string emptyLine= new string('_',dungeonWidth);
+    char verticalWall = '|';
+    char emptySpace = '_';
+    string emptyLine= new string('_',dungeonWidth-2);
+    emptyLine = verticalWall + emptyLine  + verticalWall;
+
+
+
+    char hero = '@';
+
+    int leftSide = heroX-1-1;  // -1 for wall and -1 for hero space 
+    int rightSide = dungeonWidth-heroX-1; // -1 for horizontalWall
+    
+    string herorightside = new string('_',rightSide);
+    string heroLine = new string('_',leftSide);
+    heroLine='|' + heroLine + hero + herorightside + '|';
     
 
+    
+    // the Dungeon Printing Part
 
     Console.WriteLine(horizontalWall);
 
      for (int i = 0; i < dungeonHeight-2 ; i++){
-    Console.WriteLine(emptyLine);
+       if( i != (heroY-2 ) ){
+    Console.WriteLine(emptyLine);}
+       else{ Console.WriteLine(heroLine);}
    
      }
 Console.WriteLine(horizontalWall);
