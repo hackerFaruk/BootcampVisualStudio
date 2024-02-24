a96ab992c61bb1703cf2426927f25e6c5abdc906
hackerFaruk <onuralparslan97@gmail.com>
2024-02-24 13:53:44 +0300

    csharpied

--- SecondConsoleApp/ConsoleApp.DungeonCrawler/Program.cs
+++ SecondConsoleApp/ConsoleApp.DungeonCrawler/Program.cs
@@ -3,22 +3,18 @@ using System.Collections.Generic;
 using ConsoleApp;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
-
 namespace ConsoleApp.DungeonCrawler
 {
     internal class Program
     {
-
         private static string appName = " This is Dungeon Crawlerr";
 
         static void Main(string[] args)
         {
             Console.WriteLine(appName);
             dungeonCrawlerGame.movementControls();
-
         }
     }
-
 
     public class dungeonCrawlerGame
     {
@@ -55,40 +51,43 @@ namespace ConsoleApp.DungeonCrawler
             } while (cki.Key != ConsoleKey.Escape);
         }
 
-
-
         /// <summary>
         /// IT is a better movement coantrol scheme that can employ obstacle avoidance
         /// </summary>
         /// <param name="heroX"> It holds horizontal position of hero </param>
         /// <param name="heroY"> It holds vertical position of hero </param>
-        internal static void movementControls(int heroX =4 ,int heroY=4, int dungeonX=7, int dungeonY=7 ) {
-            // it has default values of 4 4 on hero location 
-            // in here wasd will be converted to hero location but it will also prevent moveing to walls 
-            // so if dungeon x*y  maks movement range 1 to x-1 and 1 to y-1 
+        internal static void movementControls(
+            int heroX = 4,
+            int heroY = 4,
+            int dungeonX = 7,
+            int dungeonY = 7
+        )
+        {
+            // it has default values of 4 4 on hero location
+            // in here wasd will be converted to hero location but it will also prevent moveing to walls
+            // so if dungeon x*y  maks movement range 1 to x-1 and 1 to y-1
 
             ConsoleKeyInfo cki;
 
             Dictionary<string, int> movementButtons = new Dictionary<string, int>()
-        {
-            {"W", 1},
-            {"A", 2},
-            {"S", 3},
-            {"D", 4},
+            {
+                { "W", 1 },
+                { "A", 2 },
+                { "S", 3 },
+                { "D", 4 },
             };
 
-            int XMin = 2; // due to wall being in zero 
-            int XMax = dungeonX-1;
+            int XMin = 2; // due to wall being in zero
+            int XMax = dungeonX - 1;
 
-            int YMax = dungeonY-1;
+            int YMax = dungeonY - 1;
             int YMin = 2;
 
             string key;
             int command;
 
-
-
-            do {
+            do
+            {
                 Console.WriteLine("Press the Escape (Esc) key to quit: \n");
                 cki = Console.ReadKey();
                 Console.Clear();
@@ -96,11 +95,11 @@ namespace ConsoleApp.DungeonCrawler
 
                 if (movementButtons.ContainsKey(key))
                 {
-                    command= movementButtons[key];
+                    command = movementButtons[key];
                 }
                 else
                 {
-                   command= 99;
+                    command = 99;
                 }
 
                 // instead of making key.tostring in each if switch case allows one transform
@@ -116,38 +115,32 @@ namespace ConsoleApp.DungeonCrawler
                         break;
                     case 3:
                         // S pressed down movement
-                        // increased y means lower position 
+                        // increased y means lower position
                         heroY++;
                         break;
-                        case 4:
+                    case 4:
                         // S pressed right movement
                         heroX++;
                         break;
                     default:
-                        // beeps if another key pressed 
+                        // beeps if another key pressed
                         Console.Beep();
                         break;
                 }
 
-
-
-                // hero position limit check 
-                if(heroX>XMax) heroX = XMax;
-                if(heroY>YMax) heroY = YMax;
-                if(heroX<XMin) heroX = XMin;
-                if(heroY<YMin) heroY = YMin;
-
+                // hero position limit check
+                if (heroX > XMax)
+                    heroX = XMax;
+                if (heroY > YMax)
+                    heroY = YMax;
+                if (heroX < XMin)
+                    heroX = XMin;
+                if (heroY < YMin)
+                    heroY = YMin;
 
                 printDungeon(heroX, heroY);
-
             } while (cki.Key != ConsoleKey.Escape);
-
-
-
-
         }
-
-
 
         public static void printDungeon(int heroX, int heroY)
         {
@@ -188,8 +181,4 @@ namespace ConsoleApp.DungeonCrawler
             Console.WriteLine(horizontalWall);
         }
     }
-
-
-
-
 }
