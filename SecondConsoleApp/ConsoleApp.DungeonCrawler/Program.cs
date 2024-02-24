bf93a972fcb36bd57069d93160b6a5edd6117a00
hackerFaruk <onuralparslan97@gmail.com>
2024-02-24 13:12:43 +0300

    imprtoveing movemnt and dungeon system

--- SecondConsoleApp/ConsoleApp.DungeonCrawler/Program.cs
+++ SecondConsoleApp/ConsoleApp.DungeonCrawler/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using ConsoleApp;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 
 namespace ConsoleApp.DungeonCrawler
@@ -52,6 +53,98 @@ namespace ConsoleApp.DungeonCrawler
                 printDungeon(heroX, heroY);
             } while (cki.Key != ConsoleKey.Escape);
         }
+
+
+        internal static void movementControls(int heroX =4 ,int heroY=4 ) {
+            // it has default values of 4 4 on hero location 
+            // in here wasd will be converted to hero location but it will also prevent moveing to walls 
+            // so if dungeon x*y  maks movement range 1 to x-1 and 1 to y-1 
+
+            ConsoleKeyInfo cki;
+
+            Dictionary<string, int> movementButtons = new Dictionary<string, int>()
+        {
+            {"W", 1},
+            {"A", 2},
+            {"S", 3},
+            {"D", 4},
+            };
+
+             string key;
+
+
+
+
+
+            do {
+                cki = Console.ReadKey();
+
+                switch (movementButtons[cki.Key.ToString()])
+                {
+                    case 1:
+                        // W pressed upwards movement
+                        Console.WriteLine("Number is 1");
+                        break;
+                    case 2:
+                        // A pressed letf movement
+                        Console.WriteLine("Number is 2");
+                        break;
+                    case 3:
+                        // S pressed down movement
+                        Console.WriteLine("Number is 3");
+                        break;
+                        case 4:
+                        // S pressed right movement
+                        Console.WriteLine("Number is not 1, 2, or 3");
+                        break;
+                    default:
+                        // beeps if another key pressed 
+                        Console.Beep();
+                        break;
+                }
+
+
+
+                // hero position limit check 
+
+                printDungeon(heroX, heroY);
+
+            } while (cki.Key != ConsoleKey.Escape);
+
+
+
+
+
+            do
+            {
+                Console.WriteLine("Press the Escape (Esc) key to quit: \n");
+                cki = Console.ReadKey();
+                Console.Clear();
+                // since upper most line is zero this works a little diffrent in Y direction
+                if (cki.Key.ToString() == "W")
+                {
+                    heroY--;
+                }
+                else if (cki.Key.ToString() == "D")
+                {
+                    heroX++;
+                }
+                else if (cki.Key.ToString() == "S")
+                {
+                    heroY++;
+                }
+                else if (cki.Key.ToString() == "A")
+                {
+                    heroX--;
+                }
+                printDungeon(heroX, heroY);
+            } while (cki.Key != ConsoleKey.Escape);
+
+
+
+        }
+
+
 
         public static void printDungeon(int heroX, int heroY)
         {
