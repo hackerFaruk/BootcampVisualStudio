2f82df1051f52fe69b98c1205c87af6df66d527c
hackerFaruk <onuralparslan97@gmail.com>
2024-02-25 09:44:28 +0300

    added lesson practice

--- SecondConsoleApp/ConsoleApp.Start/Program.cs
+++ SecondConsoleApp/ConsoleApp.Start/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using ConsoleApp.DungeonCrawler;  //  porjeme ters tıklayıp add project refrence diyince usingde öneri olarak geldi bu 
-// DungeonCrawler game içindeki dungeon crawler static olduğu için objesisz çağırıyom 
+using ConsoleApp.DungeonCrawler; //  porjeme ters tıklayıp add project refrence diyince usingde öneri olarak geldi bu
+
+// DungeonCrawler game içindeki dungeon crawler static olduğu için objesisz çağırıyom
 
 
 
@@ -9,22 +10,53 @@ namespace ConsoleApp.Start
 {
     internal class Program
     {
+        internal static Dictionary<int, string> opertaionsList = new Dictionary<int, string>();
 
         private static string appName = " Thats Second Version of My app";
 
         static void Main(string[] args)
         {
             Console.WriteLine(appName);
+            nameChechker();
+        }
 
-         
-           
+        static void keyDetetc()
+        {
+            ConsoleKeyInfo cki;
 
+            do
+            {
+                cki = Console.ReadKey();
+                Console.Clear();
+                Console.WriteLine("şuan basılan tuş");
+                Console.WriteLine(cki.Key.ToString());
+                ;
+            } while (cki.Key != ConsoleKey.Escape);
+        }
+
+        /// <summary>
+        /// ders uygulması ders 6
+        /// </summary>
+        static void nameChechker()
+        {
+            Console.WriteLine("bi isim giriniz ");
+
+            string isimInput = Console.ReadLine();
+
+            if (isimInput.Length > 7)
+            {
+                Console.Beep();
+                Console.WriteLine("isimin cok uzun ben  sana ke diycem");
+                Console.Beep();
+            }
+            else if (isimInput == "alp")
+            {
+                Console.WriteLine("sen ben misin");
+            }
+            else
+            {
+                Console.WriteLine("akılda turması kolay isim");
+            }
         }
     }
-
-
-
-    
-
-
 }
