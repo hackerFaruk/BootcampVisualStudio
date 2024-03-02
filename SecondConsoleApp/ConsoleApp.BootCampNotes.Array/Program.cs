4e462ac9b5bd4d13dba9c8424c01eb67a5433cc1
hackerFaruk <onuralparslan97@gmail.com>
2024-03-02 09:45:43 +0300

    added list structure

---
+++ SecondConsoleApp/ConsoleApp.BootCampNotes.Array/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ConsoleApp.BootCampNotes.Array
+{
+    internal class Program
+    {
+
+        // array verileri dip dibe oluşturur array tek bir blok halinde içinde farklı verriler tutar.
+
+
+
+        // int[] sayilar = new int[2]   iki int koymalık alanı tanımlar 
+        static public int[] intValues = new int[2];
+        
+
+        // içindekileri biliyorsan süslü parantez ile eklme yapabilirsin 
+        static public int[] myNum = { 10, 20, 30, 40 };
+
+        // Array eleman sayısını ister liste ise eleman sayısı olmadan rahatça hareket eder
+        // 
+
+        static void Main(string[] args)
+        {
+            // sayilar[sayilar.lenth -1 ] son eleman
+            Console.WriteLine(myNum[myNum.Length-1]);
+        }
+
+
+    }
+}
