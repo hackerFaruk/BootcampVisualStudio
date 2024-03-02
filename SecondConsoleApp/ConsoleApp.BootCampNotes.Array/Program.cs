7d521877d05b9e1be042fdbcc43ab0645087c986
hackerFaruk <onuralparslan97@gmail.com>
2024-03-02 09:52:27 +0300

    Added diffrent methods of array defination

--- SecondConsoleApp/ConsoleApp.BootCampNotes.Array/Program.cs
+++ SecondConsoleApp/ConsoleApp.BootCampNotes.Array/Program.cs
@@ -10,19 +10,48 @@ namespace ConsoleApp.BootCampNotes.Array
 
 
         // int[] sayilar = new int[2]   iki int koymalık alanı tanımlar 
-        static public int[] intValues = new int[2];
+        static internal int[] intValues = new int[2];
         
 
         // içindekileri biliyorsan süslü parantez ile eklme yapabilirsin 
-        static public int[] myNum = { 10, 20, 30, 40 };
+        static internal int[] myNum = { 10, 20, 30, 40 };
+
+
+
+
+        static internal void ArrayMaker()
+        {
+            int[] numx;
+            numx = new int[2];
+
+
+        }
 
         // Array eleman sayısını ister liste ise eleman sayısı olmadan rahatça hareket eder
-        // 
+        // diyelim ki  kaç eleman kullancan bilmiyrosun 
+
+        static internal List<int> myList = new List<int>();
+
+        static internal void ListLister() {
+            myList.Add(1);
+            Console.WriteLine("listemin ilk hali");
+            foreach (int i in myList) { Console.WriteLine(i); }
+            myList.Add(14);
+            myList.Add(13);
+            myList.Add(12);
+            Console.WriteLine("listemin yeni hali");
+            foreach (int i in myList) { Console.WriteLine(i); }
+        }
+
+
 
         static void Main(string[] args)
         {
             // sayilar[sayilar.lenth -1 ] son eleman
             Console.WriteLine(myNum[myNum.Length-1]);
+
+            ListLister();
+          
         }
 
 
