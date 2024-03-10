cb468c355f6a3748da81fa1de9279dc8cbabd2cc
hackerFaruk <onuralparslan97@gmail.com>
2024-03-10 09:44:16 +0300

    Oyuncu numarasını alma görevi eklend i

--- NPATGame/NPATGame/GamePlay.cs
+++ NPATGame/NPATGame/GamePlay.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,8 +11,81 @@ namespace NPATGame
     {
         public char Letter;
 
+        public static char[] possibleChars =
+        {
+            'a',
+            'b',
+            'c',
+            'd',
+            'e',
+            'f',
+            'g',
+            'h',
+            'i',
+            'j',
+            'k',
+            'l',
+            'm',
+            'n',
+            'o',
+            'p',
+            'q',
+            'r',
+            's',
+            't',
+            'u',
+            'v',
+            'w',
+            'x',
+            'y',
+            'z'
+        };
 
+        public int letterAmount = possibleChars.Length;
 
-        public void Select 
+        int playerCount = 0;
+
+        Random rand = new Random();
+
+        internal void SelectLetter()
+        {
+            int randomNumber = rand.Next(letterAmount);
+            Letter = possibleChars[randomNumber];
+        }
+
+        internal void getPlayerCount()
+        {
+            Console.WriteLine("Oyuna Başlamadan Önce Lütfen Kaç kişi");
+            playerCount = ReadIntFromConsole();
+
+            if (playerCount == 0)
+            {
+                Console.WriteLine("Oyuncu olmadığı için Oyun Bitiriliyor ");
+                return;
+            }
+        }
+
+        internal int ReadIntFromConsole()
+        {
+            string? read = Console.ReadLine();
+            int result = 0;
+
+            if (read == "exit")
+            {
+                return 0;
+            }
+
+            try
+            {
+                result = int.Parse(read);
+            }
+            catch
+            {
+                Console.WriteLine(" Int vermedin, ya int ver ya exit yaz");
+                ReadIntFromConsole();
+            }
+
+            return result;
+        }
     }
 }
--- NPATGame/NPATGame/Program.cs
+++ NPATGame/NPATGame/Program.cs
@@ -1,11 +1,20 @@
-﻿namespace NPATGame
+﻿
+
+
+
+
+namespace NPATGame
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Name, Animal, Place & Thing Game");
-           
+            
+            GamePlay game1 = new GamePlay();
+            
+
+
         }
     }
 }
