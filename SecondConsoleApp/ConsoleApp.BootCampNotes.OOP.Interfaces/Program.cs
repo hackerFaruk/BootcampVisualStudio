5bc73749bdb5db4b3e58e0c9a907bc6b1ac503f5
hackerFaruk <onuralparslan97@gmail.com>
2024-03-10 10:26:43 +0300

    added polymorrhisim methods

---
+++ SecondConsoleApp/ConsoleApp.BootCampNotes.OOP.Interfaces/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Channels;
+using System.Xml;
+
+namespace ConsoleApp.BootCampNotes.OOP.Interfaces
+{
+
+
+
+    // Bu programda Interface bakcaz 
+    // diyelim ki 3 farklı tipte ödeme alan çalışnlar için bi ödeme alt yapısı kurcaz 
+    // 3 farklı ödeme tipi için calslar lazım
+
+    // freelancer çalıştığı saat başına 
+
+    // monthlyPaid aylık düzende çalışan
+
+    // projectPaid  proje bazlı anlaşma ile çalışan
+
+
+    // Hepsinde ortak olan  getPaid fonksiyonu hepsinde parayı ibana atıyor bu yüzden normal inheritlenebilir
+
+
+    // ancak hepsinin maaşını hepsaplayan farklı methodlar lazım.
+    // bunları kolayca hatırlkamak için hepsinin adının aynı olmasını sağlicaz bu interface ilel oluyr 
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+
+
+        internal class PaymentMaker()
+        {
+          internal void GetPaid()
+            {
+                Console.WriteLine("OOO maaşlar yatttıııı");
+            }
+        }
+
+
+
+    }
+}
---
+++ SecondConsoleApp/ConsoleApp.BootCampNotes.OOP.Polymorph/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ConsoleApp.BootCampNotes.OOP.Polymorph
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SmsNotification yeniMesaj = new SmsNotification();
+            EmailNotification yeniMail = new EmailNotification();
+
+            yeniMail.History();
+            yeniMesaj.Call();
+            yeniMail.SendNotification("habeer");
+
+        }
+    }
+
+    class Notification
+    {
+        // bu emthoda farklı clasllarda override edilebilir artık virtual sayesinde
+        public virtual void SendNotification(string data)
+        {
+            Console.WriteLine("dikkat dikkat haber haber ");
+        }
+
+        public void History()
+        {
+            Console.WriteLine("eski bildirimler kontrol ediliyorr");
+        }
+    }
+
+    class SmsNotification : Notification
+    {
+        public  void Call()
+        {
+            Console.WriteLine("dülülü dülülül telefon çalır ");
+        }
+    }
+
+    class EmailNotification : Notification
+    {
+        public override void SendNotification(string data)
+        {
+            Console.WriteLine("Internet Çağı Haberiiii");
+        }
+    }
+}
