7d9d3b2750f810736089dda66210684febdbc6aa
hackerFaruk <onuralparslan97@gmail.com>
2024-03-10 12:08:54 +0300

    abstract added

---
+++ SecondConsoleApp/ConsoleApp.BootCampNotes.OOP.AbstractAndAbstraction/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace ConsoleApp.BootCampNotes.OOP.AbstractAndAbstraction
+{
+
+
+    // abstraction dedinlen şeyi psuedo code gibi düşün
+    // kural belirlemek için diyoki aga bunlar olacak olamzsa kalbini kırarım diyo 
+    
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+
+
+    // bu class bir workerin sahip olması gereken özellikleri yazar 
+    abstract class BasicWorker
+    {
+        public abstract double CalculatePayment();
+
+        public abstract void Work();
+
+        public abstract string Report();
+    }
+
+
+    // takım arkadşın bir Forklift srücü yapacak diyelim senin abstract worker clasından almaso lazım dedik 
+    // kural olarak
+
+    internal class ForkliftWorker : BasicWorker
+    {
+        // eğer abstractta istenen methodları yazmasan hata verir 
+
+        public  override double  CalculatePayment()
+        {
+            Console.WriteLine("hesap yaptım maaşı");
+            return 4.4;
+
+        }
+
+        public override void Work()
+        {
+            Console.WriteLine("büznıs büznıs");
+        }
+
+        public override string Report(
+            )
+        { Console.WriteLine(" reporting for duty");
+        return "daily report"}
+
+
+    }
+
+}
