cb52ee0a52cd895d64d138ddd1eeeb97cce824e0
hackerFaruk <onuralparslan97@gmail.com>
2024-03-23 18:34:04 +0300

    cretaed base of product

---
+++ ConsoleMarket/ConsoleMarket/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMarket
+{
+    internal class Product
+    {
+        public string Id { get; private set; } = Guid.NewGuid().ToString();
+
+        // this line allows getting Guid of product from anywhere but you cant change product id
+        public double Price;
+        public string Name;
+
+        public Product(string name, double price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+    }
+}
