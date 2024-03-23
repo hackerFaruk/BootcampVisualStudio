5f5d778be14c3b509d05240723b20a33fffba804
hackerFaruk <onuralparslan97@gmail.com>
2024-03-23 20:40:57 +0300

    shops added

---
+++ ConsoleMarket/ConsoleMarket/Shop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMarket
+{
+    internal class Shop
+    {
+       public String Name {  get; private set; }   // allows every one to see name but only constructor to set it 
+       public List<Product> Products = new List<Product>();
+
+        public Shop(string name) {
+            this.Name = name;
+
+        }
+        
+
+
+    }
+}
