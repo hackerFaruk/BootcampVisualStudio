9939d7b3242e9482410f72a3c075fe578677b303
hackerFaruk <onuralparslan97@gmail.com>
2024-03-23 21:01:32 +0300

    products and sellers added

--- ConsoleMarket/ConsoleMarket/Program.cs
+++ ConsoleMarket/ConsoleMarket/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ConsoleMarket
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleMarket
 {
     internal class Program
     {
@@ -6,5 +8,32 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        static  void createSellers()
+        {
+            Product Shovel = new Product("Shovel" , 15.0);
+            Product Flash = new Product("ProFlashLight", 7.5);
+            Product Radio = new Product("Radio", 10.0);
+            Product Ladder = new Product("Ladder", 20.0);
+
+
+            Seller Company = new Seller("Company");
+            Seller Merchant = new Seller("The Merchant");
+
+
+            Company.Products.Add(Shovel);
+            Company.Products.Add(Flash);
+
+            Merchant.Products.Add(Radio);   
+            Merchant.Products.Add(Ladder);
+
+
+
+
+
+
+        }
+
+
     }
 }
