d8796273cfede20f71f547290244479b7f4e8bec
hackerFaruk <onuralparslan97@gmail.com>
2024-03-23 20:46:00 +0300

    added seller

---
+++ ConsoleMarket/ConsoleMarket/Seller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMarket
+{
+    /// <summary>
+    /// Seller has list of Products 
+    /// </summary>
+    internal class Seller
+    {
+        public String Name { get; private set; } // allows every one to see name but only constructor to set it
+        public List<Product> Products = new List<Product>();
+
+        public Seller(string name)
+        {
+            this.Name = name;
+        }
+
+        
+
+    }
+}
