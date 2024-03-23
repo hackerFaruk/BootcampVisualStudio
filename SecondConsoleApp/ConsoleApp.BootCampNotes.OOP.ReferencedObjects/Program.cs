5a5c512b87795e94e25e30e7e3a333d082307f17
hackerFaruk <onuralparslan97@gmail.com>
2024-03-23 07:13:53 +0300

    new notes for bootcamp

---
+++ SecondConsoleApp/ConsoleApp.BootCampNotes.OOP.ReferencedObjects/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp.BootCampNotes.OOP.ReferencedObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class Catagory
+    {
+        public string Name { get; set; }
+        public int id { get; set; }
+
+        public Catagory(int id, string name)
+        {
+            this.id = id;
+            this.Name = name;
+        }
+
+        public Catagory() { }
+    }
+
+    public class Product
+    {
+        string id = Guid.NewGuid().ToString(); // this creates a specific  id for each object making all unique
+    }
+}
