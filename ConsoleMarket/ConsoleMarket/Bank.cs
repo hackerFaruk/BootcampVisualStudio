3b2a8aa002ce5c2c9d49636b3118765c9b353057
hackerFaruk <onuralparslan97@gmail.com>
2024-03-24 16:19:52 +0300

    creating person

---
+++ ConsoleMarket/ConsoleMarket/Bank.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMarket
+{
+
+    public class Bank
+    {
+
+        public string Name { get; private set; }
+
+       public  Bank(string name)
+        {
+            this.Name = name;
+
+
+        }
+
+        public void openAccount()
+        {
+
+        }
+
+    }
+}
---
+++ ConsoleMarket/ConsoleMarket/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMarket
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string TCKN { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Person( string name , string  lastName) {
+        
+            this.Name = name;
+            this.LastName = lastName;
+
+            Random rnd = new Random();
+            this.TCKN = rnd.Next(10000, 10000).ToString(); 
+            this.TCKN += rnd.Next(10000, 10000).ToString(); 
+            this.TCKN += rnd.Next(1, 10).ToString(); 
+
+            this.PhoneNumber = rnd.Next(1000000, 10000000).ToString();
+
+
+        }
+    }
+}
