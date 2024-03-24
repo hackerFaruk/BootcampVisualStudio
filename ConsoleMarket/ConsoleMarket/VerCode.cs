af6fa46b4b30c0375ad9b0887c16122ecc425891
hackerFaruk <onuralparslan97@gmail.com>
2024-03-24 15:30:06 +0300

    added hasher and hash gennerator

--- ConsoleMarket/ConsoleMarket/VerCode.cs
+++ ConsoleMarket/ConsoleMarket/VerCode.cs
@@ -1,26 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ConsoleMarket
 {
     internal static class VerCode
     {
-
-
-        public static void hashTest()
+        public static string VerCodeGen()
         {
-            string input = "Hello, world!";
-            string hashedString = ComputeSHA256Hash(input);
-            Console.WriteLine("Input string: " + input);
-            Console.WriteLine("SHA256 hash: " + hashedString);
+            Random rnd = new Random();
+            string verificationCode = rnd.Next(10000, 100000).ToString(); // generetae 5 digit code
+            FileWriter("verificationCode", verificationCode);
+
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(verificationCode);
+                byte[] hashBytes = sha256.ComputeHash(inputBytes);
+
+                // Convert the byte array to a hexadecimal string
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    builder.Append(hashBytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
         }
 
-        static string ComputeSHA256Hash(string input)
+        public static string Hasher(string input)
         {
             using (SHA256 sha256 = SHA256.Create())
             {
@@ -37,13 +48,42 @@ namespace ConsoleMarket
             }
         }
 
+        private static void FileWriter(string name, string message)
+        {
+            string filePath = "../../../" + name + ".txt";
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                writer.WriteLine(message);
+            }
+        }
 
+        #region Test Codes
+        public static void hashTest()
+        {
+            string input = "Hello, world!";
+            string hashedString = ComputeSHA256Hash(input);
+            Console.WriteLine("Input string: " + input);
+            Console.WriteLine("SHA256 hash: " + hashedString);
+        }
 
+        private static string ComputeSHA256Hash(string input)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+                byte[] hashBytes = sha256.ComputeHash(inputBytes);
 
+                // Convert the byte array to a hexadecimal string
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    builder.Append(hashBytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
 
-
-
-           public static void FileTest()
+        public static void FileTest()
         {
             // Define the path for the text file
             string filePath = "../../../test.txt";
@@ -84,7 +124,8 @@ namespace ConsoleMarket
                     string line;
                     while ((line = reader.ReadLine()) != null)
                     {
-                        Console.WriteLine(line); Console.WriteLine("REadinggg");
+                        Console.WriteLine(line);
+                        Console.WriteLine("REadinggg");
                     }
                 }
                 Console.WriteLine("Text read from file successfully.");
@@ -94,8 +135,6 @@ namespace ConsoleMarket
                 Console.WriteLine($"An error occurred while reading from the file: {ex.Message}");
             }
         }
-
-
-
+        #endregion
     }
 }
