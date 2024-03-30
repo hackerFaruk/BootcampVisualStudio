c95017ef53218eb1b225a3500ac0c14e7d4ffa54
hackerFaruk <onuralparslan97@gmail.com>
2024-03-30 14:11:24 +0300

    added ui class for better ui management

---
+++ ConsoleMarket/ConsoleMarket/Ui.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMarket
+{
+    internal class Ui: IUiPresenter
+    {
+        ShopAccount account;
+        Shop shop;
+        
+
+        internal Ui(Shop shop, ShopAccount account)
+        {
+            this.shop = shop;
+            this.account = account;
+
+        }
+
+
+         public void UiPresenter()
+        {
+            Console.Clear();
+            this.shop.UiPresenter();
+            Console.WriteLine("  ");
+            this.shop.UiPresenter();
+        }
+
+        
+        
+
+    }
+}
