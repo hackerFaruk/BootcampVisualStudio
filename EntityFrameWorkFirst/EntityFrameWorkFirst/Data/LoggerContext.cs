8b92cbf322f5469ff94d753b980b2158772b6c59
hackerFaruk <onuralparslan97@gmail.com>
2024-04-09 17:24:57 +0300

    added migration

---
+++ EntityFrameWorkFirst/EntityFrameWorkFirst/Data/LoggerContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityFrameWorkFirst.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Protocols;
+
+
+namespace EntityFrameWorkFirst.Data
+{
+    public class LoggerContext : DbContext
+    {
+        public DbSet<Entry> LogEntries { get; set; }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "Server=HACQER-FARUK\\SQLEXPRESS;Database=myDatabase;User Id=sa;Password=123;";
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+    }
+
+    
+}
