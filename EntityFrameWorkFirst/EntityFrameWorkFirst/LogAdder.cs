d40b57db08929dd8e44bfca3fcbf954930f205b0
hackerFaruk <onuralparslan97@gmail.com>
2024-04-10 21:34:54 +0300

    Created changes

---
+++ EntityFrameWorkFirst/EntityFrameWorkFirst/LogAdder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// use models and data 
+using EntityFrameWorkFirst.Data;
+using EntityFrameWorkFirst.Models;
+
+// you need to create context to create new log but update database
+using LoggerContext context = new LoggerContext();
+
+namespace EntityFrameWorkFirst
+{
+    
+    public class LogAdder
+    {
+    }
+}
