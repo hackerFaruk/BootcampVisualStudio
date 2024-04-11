a2b29e089734f0ac13a1e338729f769d65b19761
hackerFaruk <onuralparslan97@gmail.com>
2024-04-11 17:14:51 +0300

    added a basic test unit

--- EntityFrameWorkFirst/EntityFrameWorkFirst/Program.cs
+++ EntityFrameWorkFirst/EntityFrameWorkFirst/Program.cs
@@ -30,15 +30,33 @@ namespace EntityFrameWorkFirst
 
 
 
-            Console.WriteLine("Hello, World!");
-            logAdder.AddNewLog("İlk not","bu not ilk notum");
-            logAdder.AddNewLog("ikinci not", "bu not ikinci notum" );
-            logAdder.AddNewLog("üçüncü not", "bu not üçünüc notum");
+          
 
-            logViewer.ViewLog();
+        
 
 
            
+
+        }
+
+
+        static void basicTest()
+        {
+            Console.WriteLine("test");
+            // this comes from LoggerContext file  which basiclly builds db
+            using LoggerContext context = new LoggerContext();
+
+            // this comes from LogAdder.cs to make easier to add new entries
+            LogAdder logAdder = new LogAdder(context);
+
+            // this comes from log vievwr
+            LogViewer logViewer = new LogViewer(context);
+            Console.WriteLine("Hello, World!");
+            logAdder.AddNewLog("İlk not", "bu not ilk notum");
+            logAdder.AddNewLog("ikinci not", "bu not ikinci notum");
+            logAdder.AddNewLog("üçüncü not", "bu not üçünüc notum");
+
+            logViewer.ViewLog();
 
         }
 
