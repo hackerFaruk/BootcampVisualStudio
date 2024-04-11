62d0d948f2a0af23fbc8c59fc149749b068c900e
hackerFaruk <onuralparslan97@gmail.com>
2024-04-11 17:38:02 +0300

    data deletion by id added

---
+++ EntityFrameWorkFirst/EntityFrameWorkFirst/DeleteById.cs
@@ -0,0 +1,45 @@
+﻿using EntityFrameWorkFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using EntityFrameWorkFirst.Data;
+
+namespace EntityFrameWorkFirst
+{
+    public class DeleteById
+    {
+
+
+
+        LoggerContext context;
+
+        public DeleteById(LoggerContext context)
+        {
+            this.context = context;
+        }
+
+        public void TitleSearch(int id, String body)
+        {
+            var entry = context.LogEntries.Where(s => s.Id == id).FirstOrDefault();
+
+            if (entry is Entry)
+            {
+                // you need to add first or default
+                context.LogEntries.Remove(entry);
+
+            }
+
+
+         
+
+
+        }
+
+
+
+    }
+}
