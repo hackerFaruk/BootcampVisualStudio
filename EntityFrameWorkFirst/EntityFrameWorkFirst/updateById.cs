79f15e67a85cbfad39a2e522cc36187f470ff94b
hackerFaruk <onuralparslan97@gmail.com>
2024-04-11 17:35:38 +0300

    Update by id ability added

---
+++ EntityFrameWorkFirst/EntityFrameWorkFirst/updateById.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using EntityFrameWorkFirst.Data;
+using EntityFrameWorkFirst.Models;
+
+namespace EntityFrameWorkFirst
+{
+    public class UpdateById
+    {
+
+
+        LoggerContext context;
+
+        public UpdateById(LoggerContext context)
+        {
+            this.context = context;
+        }
+
+        public void TitleSearch(int id, String body)
+        {
+            var entry = context.LogEntries.Where(s => s.Id ==id).FirstOrDefault();
+
+            if ( entry is Entry)
+            {
+                // you need to add first or default
+              entry.Body = body;
+
+            }
+
+
+            Console.WriteLine("updated entry");
+            Console.WriteLine($"Id : {entry.Id}");
+            Console.WriteLine($"Title : {entry.Title}");
+            Console.WriteLine($"Body : {entry.Body}");
+
+
+        }
+    }
+}
