0bafedf994dd7a708d69ebe54fa833e93a6dff3d
hackerFaruk <onuralparslan97@gmail.com>
2024-05-02 17:45:50 +0300

    DeleterAdded

---
+++ EFAppHw/EFAppHw/DeleteCategoryByID.cs
@@ -0,0 +1,37 @@
+﻿using EFAppHw.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFAppHw
+{
+    internal class DeleteByID
+    {
+
+        NorthwindContext context;
+
+        public DeleteByID(NorthwindContext context)
+        {
+            this.context = context;
+        }
+
+        public void Category(int id) {
+        
+            var entry = context.Categories.Where(s=> s.CategoryId == id).FirstOrDefault();
+
+            if (entry is Category)
+            {
+                // you need to add first or default
+                context.Categories.Remove(entry);
+
+            }
+
+
+
+        }
+
+
+    }
+}
